cbc50a27b94a5e88542e583e2a0ed16a7d1c31f9
Ziting Shen <zshen@brynmawr.edu>
2015-10-04 15:54:18 -0400

    change indent size

--- src/Word.java
+++ src/Word.java
@@ -8,59 +8,59 @@
 */
 import java.util.ArrayList;
 class Word {
-  String word;
-  ArrayList<YearCount> count = new ArrayList<YearCount>();
-  int totalFrequency = 0;
-  int totalVolume = 0;
+    String word;
+    ArrayList<YearCount> count = new ArrayList<YearCount>();
+    int totalFrequency = 0;
+    int totalVolume = 0;
 
   // constructors
-  Word(String w) {
-    word = w;
-  }
+    Word(String w) {
+        word = w;
+    }
 
-  Word(String w, int y, int f, int v) {
-    word = w;
-    add(y, f, v);
-  }
+    Word(String w, int y, int f, int v) {
+        word = w;
+        add(y, f, v);
+    }
 
   // add the data of another year to an existing object
-  public void add(int y, int f, int v) {
-    if((f < 0) || (v < 0))
-      System.out.println("Please give valid frequency and volume!");
-    else {
-      count.add(new YearCount(y, f, v));
-      totalFrequency += f;
-      totalVolume += v;
+    public void add(int y, int f, int v) {
+        if((f < 0) || (v < 0))
+            System.out.println("Please give valid frequency and volume!");
+        else {
+            count.add(new YearCount(y, f, v));
+            totalFrequency += f;
+            totalVolume += v;
+        }
     }
-  }
 
   // help print out the word
-  public String toString() {
-    return word + "\" has appeared " + totalFrequency + " times in " 
-           + totalVolume + " volumes in " + count.size() + " years.";
-  }
+    public String toString() {
+        return word + "\" has appeared " + totalFrequency + " times in " 
+        + totalVolume + " volumes in " + count.size() + " years.";
+    }
 
   // a nested class to store the year and the corresponding frequency and
   // volumn
-  private class YearCount {
-    int year;
-    int frequency;
-    int volume;
+    private class YearCount {
+        int year;
+        int frequency;
+        int volume;
 
     // constructors
-    YearCount(int y, int f, int v) {
-      year = y;
-      frequency = f;
-      volume = v;
+        YearCount(int y, int f, int v) {
+            year = y;
+            frequency = f;
+            volume = v;
+        }
     }
-  }
 
   // optional tester
-  public static void main(String[] args) {
-    Word w = new Word("goaway", 1989, 3, 2);
-    w.add(2001, 4, 1);
-    w.add(2003, -3, 2);
-    System.out.println("tester!");
-    System.out.println(w);
-  }
+    public static void main(String[] args) {
+        Word w = new Word("goaway", 1989, 3, 2);
+        w.add(2001, 4, 1);
+        w.add(2003, -3, 2);
+        System.out.println("tester!");
+        System.out.println(w);
+    }
 }
